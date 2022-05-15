6d9de30c334328d34a720da73d6cfc2b5368f6ff
Kenny Clare <kmitchellclare@gmail.com>
2022-05-15 14:58:09 -0700

    commit tree 0eceda4303778039903dbf6dda55c050aa7ba665

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-function isPalindrome(word) {
+function isPalindrome(str) {
   // Write your algorithm here
+  let splitString = str.split("");
+
+  let reverseArray = splitString.reverse();
+
+  let joinArray = reverseArray.join("");
+
+  return joinArray;
+  
 }
+
+isPalindrome("level")
+
+console.log(isPalindrome);
 
 /* 
   Add your pseudocode here
+  // 
+  use split to return a new array with each letter
+  use reverse to reserve each letter
+  use join to put each revsered letter into a string
+  return reversed string
+  add into Palindrome word
+
 */
 
 /*
   Add written explanation of your solution here
+  We created a Palindrome function that takes in a string as an argument. We then created three statements that all do different things. We used split to split each letter of our desired word into an array. We then used a reverse method to reverse our new array of splitted letters. We then used the join method to move each reserved letter into string format and returning it. And finally added in our Palindrome word.
+  //
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("level"));
 
   console.log("");
 
